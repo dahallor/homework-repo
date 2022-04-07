0b415328372c49410f96cd515cc7af303883730d
dahallor <86208957+dahallor@users.noreply.github.com>
2022-04-07 00:50:57 -0400

    added print function

---
+++ spring_2022/ai/a1/a1_base/rotator.py
@@ -0,0 +1,68 @@
+import sys
+
+DEFAULT_STATE = '12345|1234 |12354'
+
+
+class Action:
+    """
+    Represents an action that can be performed on a state.
+    """
+
+    def __init__(self):
+        """
+        Initializes an action. Additional arguments will be needed to specify the details
+        of the action. You may find it preferable to subclass this class for each of
+        the possible action types.
+        """
+        pass  # replace with your code
+
+
+class State:
+    """
+    Represents a state.
+    """
+
+    def __init__(self, string):
+        self.currentState = string
+
+    def setCurrentState(self, data):
+        self.currentState = data
+        
+    def convertToMatrix(self):
+        state = self.currentState
+        matrix = []
+        line = []
+        for i in range(len(state)):
+            if state[i] == "|":
+                matrix.append(line)
+                line = []
+                continue
+            elif state[i] == " ":
+                line.append(None)
+            elif i == len(state) - 1:
+                line.append(int(state[i]))
+                matrix.append(line)
+            else:
+                line.append(int(state[i]))
+        
+        return matrix
+
+
+if __name__ == '__main__':
+
+    cmd = sys.argv[1]
+    if cmd:
+
+        state_string = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_STATE
+        state = State(state_string)
+
+        if cmd == 'print':
+            print(state.currentState)
+        elif cmd == 'goal':
+            pass  # replace with your code
+        elif cmd == 'actions':
+            pass  # replace with your code
+        elif cmd.startswith('walk'):
+            pass  # replace with your code
+
+        state.convertToMatrix()
---
+++ spring_2022/ai/a1/a1_base/troubleshoot.py
@@ -0,0 +1,3 @@
+test_string = "12345|1234 |12345"
+print(len(test_string))
+print(test_string[10])
