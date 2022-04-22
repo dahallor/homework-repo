9d30e7e50caafade00ea2a1eaa5394ff1eca61ec
dahallor <86208957+dahallor@users.noreply.github.com>
2022-04-22 04:06:54 -0400

    kfjghskjh

--- spring_2022/ai/a2/a2_base/agent.py
+++ spring_2022/ai/a2/a2_base/agent.py
@@ -1,30 +1,76 @@
 import random
 import util
+import pdb
+from rgb import *
 
 class Node:
     def __init__(self):
         self.open = []
         self.closed = []
         self.queue = []
-        self.tree = []
 
-    def setTree(self):
-        pass
+    def createNode(self, value, pointer):
+        node = [value, pointer]
+        return node
+
+    def addToFront(self, node):
+        self.queue.insert(0, node)
+
+    def addToBack(self, node):
+        self.queue.append(node)
+
+    def deleteFromFront(self):
+        self.queue.pop(0)
+
+    def deleteFromBack(self):
+        self.queue.pop(-1)
+
 
 
 
 
 class Agent(Node):
     def __init__(self):
+        super().__init__()
+
+    def _search(self, state, parameter):
+        value = state.__str__()
+        node = self.createNode(value, None)
+        self.open.append(node)
+
+        while len(self.open) > 0:
+            possible_actions = state.actions()
+            clone = state.clone()
+            for i in range(len(possible_actions)):
+                selected_action = possible_actions[i]
+                stored_x = selected_action.x
+                stored_y = selected_action.y
+                stored_x2 = selected_action.x2
+                stored_y2 = selected_action.y2
+                
+                state.execute(selected_action)
+                result = state.__str__()
+                if result != self.closed:
+                    node = self.createNode(result, selected_action)
+                    self.open.append(node)
+                Action(stored_x, stored_y, stored_x2, stored_y2)
+
+                pdb.set_trace()
+
+
+
+
+            pass
+
+
+
+    def BFS(self, state):
+        self._search(state, "BFS")
+
+    def DFS (self, state):
         pass
 
-    def BFS(self):
-        pass
-
-    def DFS (self):
-        pass
-
-    def a_star(self):
+    def astar(self, state, h):
         pass
 
     def random_walk(self, state, n):
@@ -34,7 +80,7 @@ class Agent(Node):
             selection = random.randint(0, options-1)
             selected_action = actions[selection]
             result = state.execute(selected_action)
-            #print(state.pprint_string())
             util.pprint(result)
+            #print(state.__str__())
 
         
--- spring_2022/ai/a2/a2_base/main.py
+++ spring_2022/ai/a2/a2_base/main.py
@@ -9,13 +9,18 @@ import sys
 if __name__ == '__main__':
     
     cmd = sys.argv[1]
-    if cmd:
+    
+    state_string = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_STATE
+    state = State(state_string)
+    agent = Agent()
+    #action = Action(None, None, None, None)
+    n = 8
 
-        state_string = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_STATE
-        state = State(state_string)
-        agent = Agent()
-        n = 8
-
-        if cmd == 'random':
+    match cmd:
+        case 'random':
             agent.random_walk(state, n)
+        case "bfs":
+            agent.BFS(state)
+        case _:
+            raise Exception
 
