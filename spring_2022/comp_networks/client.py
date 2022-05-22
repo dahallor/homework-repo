15f33aeaeb69f19bcdef25dc688797773b182c80
dahallor <86208957+dahallor@users.noreply.github.com>
2022-05-22 02:06:18 -0400

    kfjfkjf

--- spring_2022/comp_networks/client.py
+++ spring_2022/comp_networks/client.py
@@ -14,8 +14,12 @@ DISCONNECT = "EXIT" #look at DFA
 DELIMIT = b'\n'
 END = b'\n\n'
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+def _assemble_session_header(USERNAME, code):
+    head = code.encode(FORMAT)
+    head += ";".encode(FORMAT)
+    head += USERNAME.encode(FORMAT)
+    head += "\n".encode(FORMAT)
+    return head
 
 def _assemble_header_encode(USERNAME, version, code):
     head = version.encode(FORMAT)
@@ -25,7 +29,7 @@ def _assemble_header_encode(USERNAME, version, code):
     head += USERNAME.encode(FORMAT)
     head += "\n".encode(FORMAT)
     return head
-
+'''
 def _assemble_header_len(USERNAME, version, code):
     head_len = str(len(version)).encode(FORMAT)
     head_len += str(";").encode(FORMAT)
@@ -34,12 +38,21 @@ def _assemble_header_len(USERNAME, version, code):
     head_len += str(len(USERNAME)).encode(FORMAT)
     head_len += str("\n").encode(FORMAT)
     return head_len
+    '''
 
-def send_header(USERNAME, version, code):
+def send_nonsession_header(USERNAME, version, code):
     head = _assemble_header_encode(USERNAME, version, code)
-    head_len = _assemble_header_len(USERNAME, version, code)
-    pdb.set_trace()
+    head_len = str(head)
+    #pdb.set_trace()
     head_len = b' ' * (HEADER - len(head_len))
+    client.send(head_len)
+    client.send(head)
+
+def send_session_header(USERNAME, code):
+    head = _assemble_session_header(USERNAME, code)
+    head_len = str(head)
+    head_len = b' ' * (HEADER - len(head_len))
+    pdb.set_trace()
     client.send(head_len)
     client.send(head)
 
@@ -53,11 +66,14 @@ def send_body(msg):
 
 
 if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
     auth = Auth()
     USERNAME = auth.authenticate()
     version = "1.0"
     code = "10"
+    send_nonsession_header(USERNAME, version, code)
     while True:
         msg = input()
-        send_header(USERNAME, version, code)
+        send_session_header(USERNAME, code)
         send_body(msg)
--- spring_2022/comp_networks/server.py
+++ spring_2022/comp_networks/server.py
@@ -8,9 +8,9 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
 DISCONNECT = "EXIT" #look at DFA
+CHATROOM = "\general"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+#=========================================================Private Helper Methods================================================================
 
 def _decode_NonSession_PDU(connection):
     try:
@@ -18,6 +18,7 @@ def _decode_NonSession_PDU(connection):
         #pdb.set_trace()
         head_len = int(len(head_len))
         head = connection.recv(head_len).decode(FORMAT)
+        head = head.strip("\n")
         header_info = head.split(";")
         pdb.set_trace()
         print(f"{header_info[2]} has entered the chat.")
@@ -39,6 +40,8 @@ def _check_Connection(msg, connected):
         print(f"blank {msg}")
     return connected
 
+#=============================================Main Public Methods================================================================================
+
 def client_handler(connection, address):
     _decode_NonSession_PDU(connection)
     connected = True
@@ -51,15 +54,19 @@ def client_handler(connection, address):
     connection.close()
 
 def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
     server.listen()
-    print(f"[LISTENING] on {SERVER}")
+    print(f"[LISTENING]...\nWelcome to {CHATROOM}!")
     while True:
-        connection, address = server.accept()
-        thread = threading.Thread(target = client_handler, args = (connection, address))
+        conn, addr = server.accept()
+        thread = threading.Thread(target = client_handler, args = (conn, addr))
         thread.start()
         #print(f"num connections: {threading.active_count() -1}")
 
 
+#=================================================================Main============================================================================
+
 if __name__ == '__main__':
-    print("[START SERVER]")
+    print("[START SERVER]...")
     start_server()
