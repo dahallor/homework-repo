8515cc461fbd29fddd607260f5d67b42bb30b80a
dahallor <86208957+dahallor@users.noreply.github.com>
2022-05-29 01:46:03 -0400

    fkasdhfkdash

--- spring_2022/comp_networks/gui.py
+++ spring_2022/comp_networks/gui.py
@@ -15,16 +15,32 @@ class GUI(Client):
         self.root.geometry('650x800')
 
     def _elements(self):
-        self.chatroom_name = tk.Label(self.root, text="Current Chatroom name here", bg=self.BG_COLOR, fg=self.TEXT_COLOR)
-        self.chatlog = tk.Label(self.root, bg=self.BG_COLOR, fg=self.TEXT_COLOR)
-        self.input_field = tk.Entry(self.root)
-        self.enter_button = tk.Button(self.input_field, text="Enter", command=self._enter_msg, bg=self.BG_COLOR, fg=self.TEXT_COLOR)
+        self.chatroom_name_frame = tk.Frame(self.root, bd=2, width=645, height=100, relief="groove")
+        self.chatroom_name_label = tk.Label(self.chatroom_name_frame, text="It's working")
+        
+        self.chatlog_frame = tk.Frame(self.root, bd=2, width=645, height=550, relief="groove", bg="white")
+        self.chatlog_label = tk.Label(self.chatlog_frame, text="It's working")
+
+        self.input_frame = tk.Frame(self.root, bd=2, width=645, height=150, relief="groove")
+        #self.input_label = tk.Label(self.input_frame)
+        self.input_field = tk.Entry(self.input_frame)
+        self.enter_button = tk.Button(self.input_frame, text="Enter", command=self._enter_msg, bg=self.BG_COLOR, fg=self.TEXT_COLOR)
+        
 
     def _grid(self):
-        self.left_col.grid(row=0, column = 0)
-        self.chatroom.grid(row=0, column = 1)
-        self.user.grid(row=1, column = 1)
-        self.enter_button.grid(row=1, column=2)
+        self.chatroom_name_label.place(relx=.5, rely=.5, anchor="c")
+        self.chatroom_name_frame.pack()
+
+        self.chatlog_label.place(relx=.01, anchor="nw")
+        self.chatlog_frame.pack()
+
+        #self.input_label.place(anchor="c")
+        self.input_frame.pack()
+        self.input_field.place(width=620, height=145, anchor="w")
+        self.enter_button.place(anchor="e")
+
+        
+        
 
     def _enter_msg(self):
         self.chatlog.configure(text=self.user.get())
