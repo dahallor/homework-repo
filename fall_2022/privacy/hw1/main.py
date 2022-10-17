dc5424aaf327f3a1d62857ef841708eb34854537
Dave H <d.a.halloran@gmail.com>
2022-10-17 19:37:57 -0400

    done

--- fall_2022/privacy/hw1/main.py
+++ fall_2022/privacy/hw1/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import sys
+import pdb
+from mech import Mechanism
+from matplotlib import pyplot as plt
 
 def parse_CSV():
     return np.genfromtxt(fname="CS590-HW1.csv", dtype=str, delimiter=",", skip_header=1)
@@ -18,12 +21,69 @@ def get_physician_fee(array):
     #physician fee freeze at column index 3
     return array[:, 3]
 
+def get_fee_sum(array):
+    return sum(array)
+
+def run_randomized_response(X, sumX):
+    gamma = 0
+    for i in range(3):
+        sumY = 0
+        for j in range(len(X)):
+            Y = Mechanism.random_response(X[j][0], gamma)
+            if Y == 1:
+                sumY += 1
+        print(f"gamma: {gamma} Total 1's in X: {sumX} Total 1's in Y:{sumY}")
+        gamma += .25
+
+def run_laplace(f):
+    e = .1
+    r = Mechanism.laplace(f, 1, e)
+    print(f"epsilon: {e} Total 1's in f: {f} Amount of Laplace Noise: {r} Total of f+r: {int(f+r)}")
+    e = 1
+    r = Mechanism.laplace(f, 1, e)
+    print(f"epsilon: {e} Total 1's in f: {f} Amount of Laplace Noise: {r} Total of f+r: {int(f+r)}")
+
+def run_guassian(f):
+    theta = .01
+    e = .1
+    r = Mechanism.guassian(f, 1, e, theta)
+    print(f"epsilon: {e} Total 1's in f: {f} Amount of Guassian Noise: {r} Total of f+r: {int(f+r)}")
+    e = 1
+    r = Mechanism.guassian(f, 1, e, theta)
+    print(f"epsilon: {e} Total 1's in f: {f} Amount of Guassian Noise: {r} Total of f+r: {int(f+r)}")
+
+def compare_mechs(f):
+    e = .1
+    theta = .01
+    laplace = []
+    guassian = []
+    y_axis_l = []
+    y_axis_g = []
+    for i in range(100):
+        laplace.append(Mechanism.laplace(f, 1, e))
+        guassian.append(Mechanism.guassian(f, 1, e, theta))
+        y_axis_l.append(1)
+        y_axis_g.append(2)
+    plt.scatter(laplace, y_axis_l, c = "blue")
+    plt.scatter(guassian, y_axis_g, c = "orange")
+    plt.show()
+
+
 if __name__ == '__main__':
     np.set_printoptions(threshold=sys.maxsize)
     array = parse_CSV()
     array = convert_array(array)
-
     fee_col = get_physician_fee(array)
-    print(sum(fee_col))
+    sumX = get_fee_sum(fee_col)
+    print(f"Total number of 1's in X: {sumX}\n")
+    
+    run_randomized_response(array, sumX)
+    print()
+    run_laplace(sumX)
+    print()
+    run_guassian(sumX)
+    print()
+    compare_mechs(sumX)
+
 
     
--- fall_2022/privacy/hw1/mech.py
+++ fall_2022/privacy/hw1/mech.py
@@ -1,25 +1,31 @@
 import random
 import numpy as np
+import pdb
 
 class Mechanism:
-    def random_response(self, X, gamma):
+    def random_response(X, gamma):
         random_num = random.random()
         truth_threshold = .5 + gamma
         if random_num >= truth_threshold:
             Y = X
         else:
             Y = 1-X
+        return Y
 
     #f = true value
     #delta = sensativity
     #epsilon = privacy parameter
     # r = random draw from laplace
-    def laplace(self, f, delta, epsilon):
+    def laplace(f, delta, epsilon):
         mu = 0
         b = delta/epsilon
         r = np.random.laplace(mu, b, 1)
-        return f + r
+        return r[0]
 
     
-    def guassian(self):
-        pass
+    def guassian(f, delta, epsilon, theta):
+        mu = 0
+        x = 1/theta
+        sigma = np.sqrt(np.log(x) * (delta/epsilon))
+        r = np.random.normal(mu, sigma, 1)
+        return r[0]
